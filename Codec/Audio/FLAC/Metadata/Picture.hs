1eaf64e1161ad982a56d2f9944f81230ecdb5f34
Mark Karpov <markkarpov92@gmail.com>
2020-04-24 13:26:34 +0200

    Format the source code with Ormolu

--- Codec/Audio/FLAC/Metadata/Picture.hs
+++ Codec/Audio/FLAC/Metadata/Picture.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module      :  Codec.Audio.FLAC.Metadata.Picture
 -- Copyright   :  © 2017–present Mark Karpov
@@ -10,25 +12,22 @@
 -- Juicy-Pixels-powered helpers to read\/write images to FLAC metadata
 -- blocks. For best player support, use PNG or JPEG (we don't provide
 -- helpers for other formats at the time anyway).
-
-{-# LANGUAGE OverloadedStrings #-}
-
 module Codec.Audio.FLAC.Metadata.Picture
-  ( retrieveImage
-  , writeJpegPicture
-  , writePngPicture )
+  ( retrieveImage,
+    writeJpegPicture,
+    writePngPicture,
+  )
 where
 
 import Codec.Audio.FLAC.Metadata
 import Codec.Picture
-import Data.Word
 import qualified Data.ByteString.Lazy as BL
+import Data.Word
 
 -- | Read specific type of picture from FLAC metadata as a 'DynamicImage'.
-
-retrieveImage
-  :: PictureType
-  -> FlacMeta (Either String DynamicImage)
+retrieveImage ::
+  PictureType ->
+  FlacMeta (Either String DynamicImage)
 retrieveImage pictureType = do
   mpicture <- retrieve (Picture pictureType)
   case mpicture of
@@ -37,37 +36,44 @@ retrieveImage pictureType = do
 
 -- | Write the given image into FLAC metadata block corresponding to
 -- specific 'PictureType'.
-
-writeJpegPicture
-  :: PictureType       -- ^ Type of picture we're writing
-  -> Word8             -- ^ Quality factor, see 'encodeJpegAtQuality'
-  -> Image PixelYCbCr8 -- ^ The picture to write
-  -> FlacMeta ()
+writeJpegPicture ::
+  -- | Type of picture we're writing
+  PictureType ->
+  -- | Quality factor, see 'encodeJpegAtQuality'
+  Word8 ->
+  -- | The picture to write
+  Image PixelYCbCr8 ->
+  FlacMeta ()
 writeJpegPicture pictureType q image =
-  Picture pictureType =-> Just PictureData
-    { pictureMimeType    = "image/jpeg"
-    , pictureDescription = ""
-    , pictureWidth       = fromIntegral (imageWidth image)
-    , pictureHeight      = fromIntegral (imageHeight image)
-    , pictureDepth       = 24
-    , pictureColors      = 0 -- non-indexed
-    , pictureData        = BL.toStrict (encodeJpegAtQuality q image)
-    }
+  Picture pictureType
+    =-> Just
+      PictureData
+        { pictureMimeType = "image/jpeg",
+          pictureDescription = "",
+          pictureWidth = fromIntegral (imageWidth image),
+          pictureHeight = fromIntegral (imageHeight image),
+          pictureDepth = 24,
+          pictureColors = 0, -- non-indexed
+          pictureData = BL.toStrict (encodeJpegAtQuality q image)
+        }
 
 -- | Write the given image into FLAC metadata block corresponding to
 -- specific 'PictureType'.
-
-writePngPicture
-  :: PictureType       -- ^ Type of picture we're writing
-  -> Image PixelRGB8   -- ^ The picture to write
-  -> FlacMeta ()
+writePngPicture ::
+  -- | Type of picture we're writing
+  PictureType ->
+  -- | The picture to write
+  Image PixelRGB8 ->
+  FlacMeta ()
 writePngPicture pictureType image =
-  Picture pictureType =-> Just PictureData
-    { pictureMimeType    = "image/png"
-    , pictureDescription = ""
-    , pictureWidth       = fromIntegral (imageWidth image)
-    , pictureHeight      = fromIntegral (imageHeight image)
-    , pictureDepth       = 24
-    , pictureColors      = 0 -- non-indexed
-    , pictureData        = BL.toStrict (encodePng image)
-    }
+  Picture pictureType
+    =-> Just
+      PictureData
+        { pictureMimeType = "image/png",
+          pictureDescription = "",
+          pictureWidth = fromIntegral (imageWidth image),
+          pictureHeight = fromIntegral (imageHeight image),
+          pictureDepth = 24,
+          pictureColors = 0, -- non-indexed
+          pictureData = BL.toStrict (encodePng image)
+        }
--- tests/Codec/Audio/FLAC/Metadata/PictureSpec.hs
+++ tests/Codec/Audio/FLAC/Metadata/PictureSpec.hs
@@ -1,30 +1,29 @@
-{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE RecordWildCards #-}
 
-module Codec.Audio.FLAC.Metadata.PictureSpec
-  ( spec )
-where
+module Codec.Audio.FLAC.Metadata.PictureSpec (spec) where
 
 import Codec.Audio.FLAC.Metadata
 import Codec.Audio.FLAC.Metadata.Picture
 import Codec.Picture
 import Control.Monad
+import qualified Data.ByteString.Lazy as BL
 import Data.Function (on)
 import System.Directory
 import System.IO
 import System.IO.Temp (withSystemTempFile)
 import Test.Hspec
-import qualified Data.ByteString.Lazy as BL
 
 spec :: Spec
 spec = around withSandbox $ do
-  describe "writeJpegPicture/retrieveImage" . forM_ [minBound..maxBound] $ \ptype ->
+  describe "writeJpegPicture/retrieveImage" . forM_ [minBound .. maxBound] $ \ptype ->
     it ("writes JPEG picture and reads it back: " ++ show ptype) $ \path -> do
       Right (ImageYCbCr8 pic) <- readJpeg "picture-samples/lenna.jpeg"
       runFlacMeta def path (writeJpegPicture ptype 127 pic)
-      let Right (ImageYCbCr8 pic'') = (decodeJpeg . BL.toStrict)
-            (encodeJpegAtQuality 100 pic)
+      let Right (ImageYCbCr8 pic'') =
+            (decodeJpeg . BL.toStrict)
+              (encodeJpegAtQuality 100 pic)
       Just PictureData {..} <- runFlacMeta def path (retrieve $ Picture ptype)
       pictureMimeType `shouldBe` "image/jpeg"
       pictureDescription `shouldBe` ""
@@ -34,7 +33,7 @@ spec = around withSandbox $ do
       pictureColors `shouldBe` 0
       Right (ImageYCbCr8 pic') <- runFlacMeta def path (retrieveImage ptype)
       pic' `imageShouldBe` pic''
-  describe "writePngPicture/retrieveImage" . forM_ [minBound..maxBound] $ \ptype ->
+  describe "writePngPicture/retrieveImage" . forM_ [minBound .. maxBound] $ \ptype ->
     it ("writes PNG picture and reads it back: " ++ show ptype) $ \path -> do
       Right (ImageRGB8 pic) <- readPng "picture-samples/lenna.png"
       runFlacMeta def path (writePngPicture ptype pic)
@@ -54,24 +53,22 @@ spec = around withSandbox $ do
 -- | Make a temporary copy of @audio-samples/sample.flac@ file and provide
 -- the path to the file. Automatically remove the file when the test
 -- finishes.
-
 withSandbox :: ActionWith FilePath -> IO ()
 withSandbox action = withSystemTempFile "sample.flac" $ \path h -> do
   hClose h
   copyFile "audio-samples/sample.flac" path
   action path
 
-imageShouldBe
-  :: (Pixel a, Show (PixelBaseComponent a), Eq (PixelBaseComponent a))
-  => Image a
-  -> Image a
-  -> Expectation
+imageShouldBe ::
+  (Pixel a, Show (PixelBaseComponent a), Eq (PixelBaseComponent a)) =>
+  Image a ->
+  Image a ->
+  Expectation
 imageShouldBe img0 img1 = do
-  (shouldBe `on` imageWidth)  img0 img1
+  (shouldBe `on` imageWidth) img0 img1
   (shouldBe `on` imageHeight) img0 img1
-  (shouldBe `on` imageData)   img0 img1
+  (shouldBe `on` imageData) img0 img1
 
 -- | A shortcut for 'defaultMetaSettings'.
-
 def :: MetaSettings
 def = defaultMetaSettings
